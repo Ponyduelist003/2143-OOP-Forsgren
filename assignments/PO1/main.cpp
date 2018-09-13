699c19da83489c08952640d8443769a723a65557
Ponyduelist003 <42812533+Ponyduelist003@users.noreply.github.com>
2018-09-13 11:54:29 -0500

    Update main.cpp

--- assignments/PO1/main.cpp
+++ assignments/PO1/main.cpp
@@ -1,3 +1,11 @@
+ /**
+ *  Course: CMPS 2143 - OOP
+ *  Assignment: P01
+ *  Purpose: Simulate the Vector class
+ * 
+ *  @author Ponyduelist003
+ *  @version 1.1 01/03/17 
+ */
 #include <iostream>
 #include "myVector.h"
 
