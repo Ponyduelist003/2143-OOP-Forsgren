0eed64ddb8914a2b2aeda75e161fb57ad8928eec
Ponyduelist003 <42812533+Ponyduelist003@users.noreply.github.com>
2018-09-14 10:52:50 -0500

    Update main.cpp

--- assignments/PO1/main.cpp
+++ assignments/PO1/main.cpp
@@ -13,5 +13,19 @@ using namespace std;
 
 int main() {
   myVector V(10);
+  V.pop_back();
+  V.push_back(13);
+  V.push_back(31);
+  V.push_back(71);
   V.print();
+  for(int i = 0; i < 1000; i++){
+    V.push_back(i + 1);
+  }
+  for(int i = 0; i < 998; i++){
+    V.pop_back();
+  }
+  cout << V.size() << endl;
+  V.print();
+
+  return 0;
 }
