dda40a9ddd86302731af91a6c78ad4fb1a8d0545
Ponyduelist003 <42812533+Ponyduelist003@users.noreply.github.com>
2018-09-14 13:57:41 -0500

    Update myVector.h

--- assignments/PO1/myVector.h
+++ assignments/PO1/myVector.h
@@ -1,19 +1,22 @@
+#pragma once
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
-class myVector{
+class myVector {
 private:
-  int *theVector;
-  int vSize;
-  int initSize;
-  int slotsFull = 0;
-  double percentFull = 0;
+	int *theVector;
+	int *diffVector;
+	int vSize;
+	int initSize;
+	int slotsFull = 0;
+	double percentFull = 0;
 public:
-  myVector(int size);
-  myVector();
-  void push_back(int val);
-  int pop_back();
-  int size();
-  void print();
+	myVector(int size);
+	myVector();
+	void push_back(int val, ofstream& outfile);
+	int pop_back(ofstream& outfile);
+	int size();
+	void print(ofstream& outfile);
 };
