6d4a7d73e42c45122df4ed90ec8370f67f9ce8f3
Ponyduelist003 <42812533+Ponyduelist003@users.noreply.github.com>
2018-10-04 22:42:15 -0500

    Update GameOfLife.cpp

--- assignments/AO4/GameOfLife.cpp
+++ assignments/AO4/GameOfLife.cpp
@@ -3,83 +3,77 @@
 using namespace sf;
 
 struct golCell {
-  bool isAlive;
-  int neighborCount;
-  RectangleShape Rect;
-  int Width;
-  int Height;
+	bool isAlive;
+	int neighborCount;
+	RectangleShape Rect;
+	int Width;
+	int Height;
 
-  golCell(){
-      Width = 10;
-      Height = 10;
-      Rect.setSize(sf::Vector2f(Width, Height));
-      Rect.setFillColor(Color::Green);
-      Rect.setOutlineColor(Color::Black);
-      Rect.setOutlineThickness(1);
-  }
+	golCell() {
+		Width = 10;
+		Height = 10;
+		Rect.setSize(sf::Vector2f(Width, Height));
+		Rect.setFillColor(Color::Green);
+		Rect.setOutlineColor(Color::Black);
+		Rect.setOutlineThickness(1);
+	}
 
-  void setCellPos(int x,int y){
-      Rect.setPosition(x*Width, y*Height);
-  }
+	void setCellPos(int x, int y) {
+		Rect.setPosition(x*Width, y*Height);
+	}
 
 };
 
-struct gameOfLife{
-    int Width;
-    int Height;
-    int FrameCount;
-    int FrameRate; 
-    RenderWindow Window;
-    golCell** World;
-    gameOfLife(int width,int height) {
-        Width = width;
-        Height = height;
-        FrameCount = 0;
-        FrameRate = 5;
-        Window.create(VideoMode(Width, Height), "Game of Life");
+struct gameOfLife {
+	int Width;
+	int Height;
+	int FrameCount;
+	int FrameRate;
+	RenderWindow Window;
+	golCell** World;
+	gameOfLife(int width, int height) {
+		Width = width;
+		Height = height;
+		FrameCount = 0;
+		FrameRate = 5;
+		Window.create(VideoMode(Width, Height), "Game of Life");
 
-        World = new golCell*[height];
+		World = new golCell*[height];
 
-        for(int i=0;i<height;i++){
-            World[i] = new golCell[width];
-            for(int j=0;j<width;j++){
-                World[i][j].setCellPos(i,j);
-            }
-        }
-    }
+		for (int i = 0; i < height; i++) {
+			World[i] = new golCell[width];
+			for (int j = 0; j < width; j++) {
+				World[i][j].setCellPos(i, j);
+			}
+		}
+	}
 
-    void drawWorld(){        
-        Window.clear();
-        for(int i=0;i<Height;i++){
-            for(int j=0;j<Width;j++){
-                Window.draw(World[i][j].Rect);
-            }
-        }
-        Window.display();
-    }
+	void drawWorld() {
+		Window.clear();
+		for (int i = 0; i < Height; i++) {
+			for (int j = 0; j < Width; j++) {
+				Window.draw(World[i][j].Rect);
+			}
+		}
+		Window.display();
+	}
 };
 
-
-
-int main(){
-
-    gameOfLife Gol(1000,1000);
-    while (Gol.Window.isOpen())
-    {
-        Event event;
-        while (Gol.Window.pollEvent(event))
-        {
-            if (FrameCount % FrameRate == 0){
-              Gol.drawWorld();
-            }
-            else if (event.type == Event::Closed){
-              Gol.Window.close();
-            }
-            Gol.FrameCount++
-        }
-
-
-    }
-
-    return 0;
+int main() {
+	gameOfLife Gol(1000, 1000);
+	while (Gol.Window.isOpen())
+	{
+		Event event;
+		while (Gol.Window.pollEvent(event))
+		{
+			if (Gol.FrameCount % Gol.FrameRate == 0) {
+				Gol.drawWorld();
+			}
+			else if (event.type == Event::Closed) {
+				Gol.Window.close();
+			}
+			Gol.FrameCount++;
+		}
+	}
+	return 0;
 }
