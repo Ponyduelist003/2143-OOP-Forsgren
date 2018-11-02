eddcec7bc27463304400a3bd1ad5508dffef975c
Ponyduelist003 <42812533+Ponyduelist003@users.noreply.github.com>
2018-11-01 20:28:17 -0500

    Update Counter.cpp

--- assignments/AO5/Counter.cpp
+++ assignments/AO5/Counter.cpp
@@ -3,21 +3,67 @@
 
 // https://www.sfml-dev.org/tutorials/2.5/system-time.php
 
-using namespace std;
+using namespace sf;
 
 class Counter {
-	int start;
-	int end;
+	Clock clock;
+	Time start;
+	Time end;
+	Time elapsed;
 
+public:
+
+	Counter();
+	Counter(int s, int e);
+	Clock getClock();
+	Time getElapsed();
+	Time getStart();
+	Time getEnd();
+	void setElapsed();
 };
+
+Counter::Counter() {
+	start = seconds(0);
+	end = seconds(0);
+}
+
+Counter::Counter(int s, int e) {
+	start = seconds(s);
+	end = seconds(e);
+}
+
+Clock Counter::getClock() {
+	return clock;
+}
+
+Time Counter::getElapsed() {
+	return elapsed;
+}
+
+Time Counter::getEnd() {
+	return end;
+}
+
+Time Counter::getStart() {
+	return start;
+}
+
+void Counter::setElapsed() {
+	elapsed = clock.getElapsedTime();
+}
 
 int main()
 {
 	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
 
-	Counter myClock1(5, 0); // start at 5 and decrement to 0
+	//sf::Clock clock;
+	//sf::Time start;
+	//sf::Time end;
+	//sf::Time elapsed;
+	//start = sf::seconds(50);
+	//end = sf::seconds(50);
 
-	Counter myClock2(0, 5); // start at 0 and increment to 5
+	Counter counter(5, 5);
 
 	while (window.isOpen())
 	{
@@ -28,13 +74,17 @@ int main()
 				window.close();
 		}
 
+		counter.setElapsed();
+		std::cout << "Incrementing Clock: " << (int) counter.getElapsed().asSeconds()<< std::endl;
+
+		counter.getElapsed() = counter.getClock().getElapsedTime();
+		std::cout << "Decrementing Clock: " << (int) (counter.getStart().asSeconds() - counter.getElapsed().asSeconds()) << std::endl;
+		if (counter.getStart().asSeconds() - counter.getElapsed().asSeconds() <= 0) {
+			window.close();
+		}
 		window.clear();
 		//window.draw(shape);
 		window.display();
-
-		if (myClock1.isFinished()) {
-			window.close();
-		}
 	}
 
 	return 0;
